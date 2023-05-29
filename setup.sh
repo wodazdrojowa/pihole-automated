9b391b0e43cfa3ba3a3a3bb7ce6dbe1321de1592
wodazdrojowa <wodazdrojowa@gmail.com>
2023-05-30 01:09:52 +0200

    ok

--- setup.sh
+++ setup.sh
@@ -1,8 +1,15 @@
 #!/bin/bash
+
+gcloud_list_instances () {
+# use:  gcloud_list_instances (status, zone)
+gcloud compute instances list --filter="status=$1" --format="value(name,$2,creation_timestamp)" |
 gcloud compute instances list --filter="status=terminated" --format="value(name,zone,creation_timestamp)" |
 while read name zone creation_timestamp
 do
   echo "Instance name: $name"
   echo "Created on $creation_timestamp"
-  gcloud compute instances delete $name --zone=$zone --quiet
+  ##################### gcloud compute instances delete $name --zone=$zone --quiet
 done
+}
+
+gcloud_list_instances (terminated, us-west1)
