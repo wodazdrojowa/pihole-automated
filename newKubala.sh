fa985dae6cfe6d6e3c4dee9b4902266b65f76f31
wodazdrojowa <wodazdrojowa@gmail.com>
2024-05-05 21:00:55 +0200

    ok

---
+++ newKubala.sh
@@ -0,0 +1,16 @@
+#!/usr/bin/bash
+howManyProjects=$(./countProjects.sh | wc -l)
+echo "Number of projects: " ${howManyProjects}
+
+if [ "$howManyProjects" -eq 0 ]; then
+    echo "There are no projects, i'll create one for you."
+    echo "creating project"
+elif [ "$howManyProjects" -eq 1 ]; then
+    echo "There is one project, let's go forward."
+    echo "next steps"
+else
+    echo "There is more than one project, don't know what to do, exiting."
+fi
+
+    
+#gcloud compute instances list
